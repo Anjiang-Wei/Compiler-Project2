4d051a435c79cfa32857d6e582f47cc8bf6e2471
Anjiang-Wei <1020681930@qq.com>
2020-06-19 02:21:51 -0700

    Transform test for case 6 is passed

--- src/transformtest.cpp
+++ src/transformtest.cpp
@@ -1,8 +1,8 @@
 #include<iostream>
 #include"transform.h"
 using namespace std;
-#define A_row 3
-#define A_column 5
+#define A_row 4
+#define A_column 7
 void Print(matrix M) {
     for(int i=0;i<M.n_rows;i++) {
         cout<<"row "<<i<<": ";
@@ -14,13 +14,20 @@ void Print(matrix M) {
 }
 
 int main() {
-    int a[] = {17,9,3,2,5,4,8,6,7,7,13,5,3,23,6};
+    int a[] = {1, 0, 0, 0 ,0, 0, 0, 
+               0, 0, 0, 0, 1, 0, 0,
+               0, 0, 1, 0, 0, 1, 0, 
+               0, 0, 0, 1, 0, 0, 1};
     matrix A(A_row,A_column);
     matrix U(A_row,A_row);
     matrix S(A_row,A_column);
     matrix V(A_column,A_column);
 
     A.insert(a);
+
+    cout << "A is intialized as" << endl;
+    Print(A);
+
     U = identity(U.n_rows);
     V = identity(V.n_rows);
     S = A;
@@ -31,4 +38,8 @@ int main() {
     Print(R);
     cout<<"--------------------"<<endl<<"S="<<endl;
     Print(S);
+    cout<<"--------------------"<<endl<<"U="<<endl;
+    Print(U);
+    cout<<"--------------------"<<endl<<"V="<<endl;
+    Print(V);
 }
